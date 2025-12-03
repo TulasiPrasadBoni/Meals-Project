c326cdef6b201f08248ca071c72dd96fdf85a59a
TulasiPrasadBoni <tulasiprasadboni@gmail.com>
2025-12-03 10:14:21 +0530

    Commit 2

---
+++ Meals Project/script.js
@@ -0,0 +1,14 @@
+// ===== BASIC ELEMENTS =====
+const categoriesGrid = document.getElementById('categoriesGrid');
+const categoryList = document.getElementById('categoryList');
+const sidebar = document.getElementById('sidebar');
+const hamburger = document.getElementById('hamburger');
+const closeSidebar = document.getElementById('closeSidebar');
+const searchBtn = document.getElementById('searchBtn');
+const searchInput = document.getElementById('searchInput');
+const mainContent = document.getElementById('mainContent');
+
+// ===== SHOW SIDEBAR =====
+hamburger.onclick = () => { sidebar.style.display = 'flex'; }
+closeSidebar.onclick = () => { sidebar.style.display = 'none'; }
+
